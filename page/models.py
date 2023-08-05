304aeab737121d777a852b3e00776a0aab0c3456
Igor Esteves de Oliveira <igoreoliveira@gmail.com>
2023-08-05 19:31:45 -0300

    WIP Adiciona campo pra css

--- page/models.py
+++ page/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Page (models.Model):
     slug = models.SlugField(null=False, blank=False)
     title = models.TextField(null=False, blank=True, verbose_name='Título da página')
+    style_css = models.TextField(null=False, blank=True, verbose_name='Estilo em CSS')
     def __str__(self):
         return self.slug
     
